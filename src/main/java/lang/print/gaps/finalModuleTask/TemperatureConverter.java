ec3300df59362e3458033e17e210453ff1bf23bf
Apoffis <abc22_08@mail.ru>
2022-10-20 20:28:06 +0400

    taskFinal commit

--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
+
     public void toFahrenheit(int temperatureCelsius){
+        float index = 9 / (float)5;
+        System.out.println(temperatureCelsius * index + 32);
     }
 }
